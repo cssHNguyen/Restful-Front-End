ec93104c4f7604bff1db921860a049f3b1e1c992
cssHNguyen <csueb.css.hnguyen@gmail.com>
2015-05-15 17:17:18 -0700

    Director/Folder Change
    -Don’t want other to know this project is specifically for TATCHA.

--- main.php
+++ main.php
@@ -3,20 +3,6 @@
  * Hong P. Nguyen
  * Tatcha Code Challenge
  */
-    //$var1 = $_GET['http://api.tatcha.com/shop/api/rest/products'];
-
-
-    //From PHP Documentation
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://api.tatcha.com/shop/api/rest/products/");
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-
-    // grab URL and pass it to the browser
-    curl_exec($ch);
-
-    // close cURL resource, and free up system resources
-    curl_close($ch);
-
 
 
 
