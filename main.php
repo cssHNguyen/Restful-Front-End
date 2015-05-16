d0dd2386c4a108eb6866b247d7584ba23a05c33e
cssHNguyen <csueb.css.hnguyen@gmail.com>
2015-05-15 17:26:14 -0700

    Data Parsed
    -The JSON that was fetched is not in a workable format. Will need to
    identify their key-pairs.

--- main.php
+++ main.php
@@ -1,10 +1,46 @@
 <?php
-/*
- * Hong P. Nguyen
- * Tatcha Code Challenge
- */
+    /*
+     * Hong P. Nguyen
+     * Tatcha Code Challenge
+     */
 
+    $endpoint = 'http://api.tatcha.com/shop/api/rest/products/';
 
+    //  Initiate curl
+    $ch = curl_init();
 
+    //Make sure Json is returned
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
 
+    // Make sure it returns JSON
+    curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+
+    // Disable SSL verification
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    // Will return the response, if false it print the response
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Set the url
+    curl_setopt($ch, CURLOPT_URL,$endpoint);
+
+    // Execute
+    $result=curl_exec($ch);
+
+    // Closing
+    curl_close($ch);
+
+    //Parse that mess into a PHP variable
+    $myResult = json_decode($result);
+    /*
+    echo $myResult->{"2"}->{"entity_id"};
+    echo "\n";
+    echo $myResult->{"2"}->{"sku"};
+    echo "\n";
+    echo $myResult->{"2"}->{"short_description"};
+    echo "\n";
+    */
+    //echo
+    //echo $result;
+    echo json_encode($myResult, JSON_PRETTY_PRINT);
 ?>
